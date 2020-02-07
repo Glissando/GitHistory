855034948d0121502990712b991927763b34fe70
Glissando <justinjoelward@gmail.com>
2020-02-06 20:12:55 -0600

    GLView added

---
+++ android/app/src/main/java/host/exp/exponent/glview.java
@@ -0,0 +1,16 @@
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class GLView extends ReactContextBaseJavaModule {
+
+    @ReactMethod
+    public void GetStatus() {
+
+    }
+}
